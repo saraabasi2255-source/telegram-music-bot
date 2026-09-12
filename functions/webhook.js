365ee8c6f99cd41b7f42bb8fde6ee64959b064cd
saraabasi2255-source <saraabasi2255@gmail.com>
2026-09-12 12:53:26 +0330

    Update welcome message in webhook.js

--- functions/webhook.js
+++ functions/webhook.js
@@ -89,7 +89,7 @@ async function handleMessage(msg, env) {
     await sendMessage(
       env,
       chatId,
-      "سلام، به بات NivaroMusic خوش اومدی🎉
+      "/n سلام، به بات NivaroMusic خوش اومدی🎉
 
 کافیه اسم آهنگ یا نام خواننده رو بفرستی تا پیدا کنم و برات بفرستم 🎧"
     );
